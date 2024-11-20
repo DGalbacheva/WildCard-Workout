4afea117e6541588a8b184bcbe94b422c83f06be
DGalbacheva <dglbacheva@gmail.com>
2024-11-20 13:41:31 +0300

    Timer created.

--- WildCardWorkout/CardSelectionViewController.swift
+++ WildCardWorkout/CardSelectionViewController.swift
@@ -13,14 +13,27 @@ class CardSelectionViewController: UIViewController {
     @IBOutlet var buttons: [UIButton]!
     @IBOutlet var cardImageView: UIImageView!
     
+    var cards: [UIImage] = []
+    
+    var timer: Timer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        startTimer()
 
         for button in buttons {
             button.layer.cornerRadius = 8
         }
     }
 
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+    }
+    
+    @objc
+    func showRandomImage() {
+        cardImageView.image = cards.randomElement() ?? UIImage(named: "AC")
+    }
 
     @IBAction func stopButtonTapped(_ sender: UIButton) {
     }
